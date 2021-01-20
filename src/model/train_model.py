505283769c236ad5454c00a9438d25cad306fda4
RafaelPinto <rafaelpinto.ve@gmail.com>
2021-01-20 05:53:38 -0600

    Change competition scoring function to accept pandas series as input

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from sklearn.metrics import accuracy_score, f1_score
+
 from src.definitions import ROOT_DIR
 
 
@@ -7,15 +9,42 @@ def score(y_true, y_pred):
     """
     Competition scoring function.
 
+    Parameters
+    ----------
+    y_true : pandas.Series
+        Ground truth (correct) target values.
+    y_pred: pandas.Series
+        Estimated targets as returned by a classifier.
+
+    Returns
+    ----------
+    float
+        2020 FORCE ML lithology competition custome score.
+
     """
     scoring_matrix_path = ROOT_DIR / 'data/external/penalty_matrix.npy'
     scoring_matrix = np.load(scoring_matrix_path)
 
     S = 0.0
-    y_true = y_true.astype(int)
-    y_pred = y_pred.astype(int)
 
-    for i in range(0, y_true.shape[0]):
-        S -= scoring_matrix[y_true[i], y_pred[i]]
+    for true_val, pred_val in zip(y_true, y_pred):
+        S -= scoring_matrix[true_val, pred_val]
 
     return S/y_true.shape[0]
+
+
+def show_evaluation(y_true, y_pred):
+    """
+    Print model performance and evaluation.
+
+    Parameters
+    ----------
+    y_true : pandas.Series
+        Ground truth (correct) target values.
+    y_pred: pandas.Series
+        Estimated targets as returned by a classifier.
+
+    """
+    print(f'Competition score: {score(y_true, y_pred)}')
+    print(f'Accuracy: {accuracy_score(y_true, y_pred)}')
+    print(f'F1: {f1_score(y_true, y_pred, average="weighted")}')
