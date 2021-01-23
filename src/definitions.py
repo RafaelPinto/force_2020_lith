70f72ba2071905b51326c220d5f905020352ba98
RafaelPinto <rafaelpinto.ve@gmail.com>
2021-01-23 11:44:14 -0600

    Add lithology value to scoring matrix index mapping to the definitions file

--- src/definitions.py
+++ src/definitions.py
@@ -1,3 +1,18 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
+
+LITHOLOGY_ORDINAL_MAP = {
+        30000: 0,
+        65030: 1,
+        65000: 2,
+        80000: 3,
+        74000: 4,
+        70000: 5,
+        70032: 6,
+        88000: 7,
+        86000: 8,
+        99000: 9,
+        90000: 10,
+        93000: 11
+    }
