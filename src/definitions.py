dd4c07fc968ffe8de5a423e10552951107aee606
RafaelPinto <rafaelpinto.ve@gmail.com>
2021-01-28 06:35:38 -0600

    Change lithology code to ordinal mapping variable name, and add additional lithology mappings to definitions

--- src/definitions.py
+++ src/definitions.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 
-LITHOLOGY_ORDINAL_MAP = {
+KEYS_TO_ORDINAL = {
         30000: 0,
         65030: 1,
         65000: 2,
@@ -16,3 +16,22 @@ LITHOLOGY_ORDINAL_MAP = {
         90000: 10,
         93000: 11
     }
+
+KEYS_TO_LITHOLOGY = {30000: 'Sandstone',
+                     65030: 'Sandstone/Shale',
+                     65000: 'Shale',
+                     80000: 'Marl',
+                     74000: 'Dolomite',
+                     70000: 'Limestone',
+                     70032: 'Chalk',
+                     88000: 'Halite',
+                     86000: 'Anhydrite',
+                     99000: 'Tuff',
+                     90000: 'Coal',
+                     93000: 'Basement'}
+
+ORDINAL_TO_KEYS = {value: key for key, value in  KEYS_TO_ORDINAL.items()}
+
+ORDINAL_TO_LITHOLOGY = {}
+for ordinal_key, key in ORDINAL_TO_KEYS.items():
+    ORDINAL_TO_LITHOLOGY[ordinal_key] = KEYS_TO_LITHOLOGY[key]
